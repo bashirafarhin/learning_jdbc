52737f38274c9b4647e721eeef17b5f8511efa3a
bashirafarhin <farhinbashira@gmail.com>
2025-12-27 09:32:19 +0530

    removed unnecessary mysql dependency

--- LearningJDBCImplementation/src/main/java/com/example/LearningJDBCImplementation/util/DBConnection.java
+++ LearningJDBCImplementation/src/main/java/com/example/LearningJDBCImplementation/util/DBConnection.java
@@ -7,17 +7,13 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-//private static final String URL =
-//        "jdbc:mysql://localhost:3306/testdb";
-//    private static final String USER = "root";
-//    private static final String PASSWORD = "bashira@2103";
-private static final String URL = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=MySQL";
+    private static final String URL = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=MySQL";
     private static final String USER = "sa";
     private static final String PASSWORD = "";
 
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("MySQL Driver not found", e);
         }
